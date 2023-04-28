47a9be8257c20064f7d04c9ed4bf99164832e76a
DairyUSMilk <117781521+DairyUSMilk@users.noreply.github.com>
2023-04-27 21:39:09 -0400

    All ingredientsDB functions
    Added all ingredients functions for database.

---
+++ data/ingredients.js
@@ -0,0 +1,91 @@
+import {ingredients} from './mongoCollections.js'
+import verification from '../public/js/verification.js';
+
+export const ingredientMethods = {
+    async createIngredient(
+        name,
+        flavors,//includes generalCuisine specifications
+        quantity,
+        measurement,
+    ){
+        name = verification.checkOnlyWordsString(name, 'name');
+        flavors = verification.checkOnlyWordsStringArray(flavors, 'flavors');
+        quantity = verification.checkNumber(quantity, 'quantity');
+        measurement = verification.checkString(measurement, 'measurement');
+        //need to check if measurement is a valid measurement but we'll do that later
+        
+        const ingredient = {
+            name : name,
+            flavors : flavors,
+            quantity : quantity,
+            measurement : measurement,
+        };
+
+        let ingredientCollection = await ingredients();
+        let insertInfo = await ingredientCollection.insertOne(ingredient);
+        if (insertInfo.insertedCount === 0) throw 'Could not add ingredient';
+        let newId = insertInfo.insertedId;
+        const newIngredient = await this.getIngredientById(newId);
+        return newIngredient;
+    },
+
+    async getIngredientById(id){
+        id = verification.checkId(id, 'id');
+        const ingredientCollection = await ingredients();
+        const ingredient = await ingredientCollection.findOne({_id : id});
+        if (ingredient === null) throw 'No ingredient with that id';
+        return ingredient;
+    },
+
+    async getAllIngredients(){
+        const ingredientCollection = await ingredients();
+        const ingredientList = await ingredientCollection.find({}).toArray();
+        return ingredientList;
+    },
+
+    async getAllIngredientsIdAndName(){
+        const ingredientCollection = await ingredients();
+        const ingredientList = await ingredientCollection.find({}, {name: 1, _id: 1}).toArray();
+        return ingredientList;
+    },
+
+    async updateIngredient(
+        id,
+        name,
+        flavors,//includes generalCuisine specifications
+        quantity,
+        measurement,
+    ){
+        id = verification.checkId(id, 'id');
+        name = verification.checkOnlyWordsString(name, 'name');
+        flavors = verification.checkOnlyWordsStringArray(flavors, 'flavors');
+        quantity = verification.checkNumber(quantity, 'quantity');
+        measurement = verification.checkString(measurement, 'measurement');
+        //need to check if measurement is a valid measurement but we'll do that later
+
+        //need to add a check for the url
+        let ingredientCollection = await ingredients();
+        let query = {_id : id};
+        let updateCommand = {
+            $set : {
+                name : name,
+                flavors : flavors,
+                quantity : quantity,
+                measurement : measurement
+            }
+        };
+        let updateInfo = await ingredientCollection.updateOne(query, updateCommand);
+        if (updateInfo.modifiedCount === 0) throw 'Could not update ingredient';
+        return await this.getIngredientById(id);
+    },
+
+    async deleteIngredient(id){
+        id = verification.checkId(id, 'id');
+        const ingredientCollection = await ingredients();
+        let ingredientName = await this.getIngredientById(id).name;
+        let deleted = await ingredientCollection.deleteOne({_id : id});
+        if (deleted.deletedCount === 0) throw 'Could not delete ingredient';
+        return {ingredientName, deleted : true};
+    }
+
+}
