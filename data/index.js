54796e0075d313cd00f2c2146629f624c35149b5
DairyUSMilk <darius.truong@gmail.com>
2023-04-29 19:18:08 -0400

    Update index.js
    Added ingredient/recipes/user db methods to index

---
+++ data/index.js
@@ -0,0 +1,7 @@
+import ingredientsMethods from './ingredients';
+import recipesMethods from './recipes';
+import usersMethods from './users';
+
+export const ingredientsData = ingredientsMethods;
+export const recipesData = recipesMethods;
+export const usersData = usersMethods;
