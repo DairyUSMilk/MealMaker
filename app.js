365443b9a0eb28b871e10ecbac886102754d1b0e
therman77 <127142043+therman77@users.noreply.github.com>
2023-05-02 16:51:27 -0400

    Updated app.js; using csprng for session secret

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+const app = express();
+
+import { randomBytes } from 'crypto';
+import session from 'express-session';
+import exphbs from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+import configRoutesFunction from './routes/index.js';
+/* { Import Middleware Here } */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + '/public');
+
+app.use('/public', staticDir);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(session({
+    name: 'MealMakerAuthCookie',
+    secret: randomBytes(160, 36),
+    resave: false,
+    saveUninitialized: false
+}))
+
+/* { Use Middleware Here } */
+
+configRoutesFunction(app);
+
+app.listen(3000, () => {
+    console.log('MealMaker routes will be running on http://localhost:3000');
+});
