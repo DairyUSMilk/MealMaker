38022cae5d794b7843d7a29e4c80aea4c63b0baf
Trent Zeller <trentzeller15@gmail.com>
2023-05-03 15:07:09 -0400

    Fixed error in recipes and changed views to correctly send to home

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,48 @@
-import ingredientsRoutes from './ingredients.js';
-import recipesRoutes from './recipes.js';
-import usersRoutes from './users.js';
-import loginRoutes from './login.js';
-import registerRoutes from './register.js';
-import express from 'express';
+import ingredientsRoutes from "./ingredients.js";
+import recipesRoutes from "./recipes.js";
+import usersRoutes from "./users.js";
+import loginRoutes from "./login.js";
+import registerRoutes from "./register.js";
+import express from "express";
 
 const constructorMethod = (app) => {
-    // TODO
-    app.get('/', (req, res) => {
-      /*
+  // TODO
+  app.get("/", (req, res) => {
+    /*
       home should look different for each type of user
       we can either make multiple home pages, or have one 
       home page that renders differently based on user type
       */
-      res.render('home', { title: 'Home' });
-    });
+    res.render("home", { title: "Home" });
+  });
 
-    app.use('/ingredients', ingredientsRoutes);
-    //if logged in, uses the ingredients stored in the user document in database
-    //if not logged in, uses the ingredients stored in the session
-    
-    app.use('/recipes', recipesRoutes);
+  app.use("/ingredients", ingredientsRoutes);
+  //if logged in, uses the ingredients stored in the user document in database
+  //if not logged in, uses the ingredients stored in the session
 
-    app.use('/user', usersRoutes);
-    /*
+  app.use("/recipes", recipesRoutes);
+
+  app.use("/user", usersRoutes);
+  /*
     this shouldn't be accessible to non-users,
     so we need middleware to check if user is logged in
     */
 
-    app.use('/register', registerRoutes);
-    //middleware here to check if user is logged in
-    //if so, redirect to home page
+  app.use("/register", registerRoutes);
+  //middleware here to check if user is logged in
+  //if so, redirect to home page
 
-    app.use('/login', loginRoutes);
-    //might want to have /login be login, not /users/login
+  app.use("/login", loginRoutes);
+  //might want to have /login be login, not /users/login
 
-    app.get('/logout', (req, res) => {
-      req.session.destroy();
-      res.render('logout', { title: 'Logout' })
-    });
-    
-    
+  app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.render("logout", { title: "Logout" });
+  });
 
-    app.use('*', (req, res) => {
-      res.status(404).json({ error: 'Route Not found' });
-    });
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Route Not found" });
+  });
 };
 
 export default constructorMethod;
--- routes/recipes.js
+++ routes/recipes.js
@@ -4,83 +4,133 @@ import verification from "../public/js/verification.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router
+  .get("/", async (req, res) => {
     try {
-        const recipes = await recipesData.getRecipesByFilter(req.body.filter);
-        return res.json(recipes);
+      const recipes = await recipesData.getRecipeByFilter(req.body.filter);
+      return res.render("recipes");
     } catch (e) {
-        res.status(500).send();
+      res.status(500).send();
     }
-}).post("/", async (req, res) => {
+  })
+  .post("/", async (req, res) => {
     const recipeInfo = req.body;
     if (!recipeInfo) {
-        res.status(400).json({ error: "You must provide data to create a recipe" });
-        return;
+      res
+        .status(400)
+        .json({ error: "You must provide data to create a recipe" });
+      return;
     }
     try {
-        recipeInfo.title = verification.checkOnlyWordsString(recipeInfo.title);
-        recipeInfo.userId = verification.checkId(recipeInfo.userId);
-        recipeInfo.flavors = verification.checkOnlyWordsStringArray(recipeInfo.flavors);
-        recipeInfo.servings = verification.checkNumber(recipeInfo.servings);
-        
-        if(recipeInfo.imageURL) recipeInfo = verification.checkURL(recipeInfo.imageURL);
-        recipeInfo.ingredients = verification.checkStringArray(recipeInfo.ingredients);
-        recipeInfo.instructions = verification.checkOnlyWordsStringArray(recipeInfo.instructions);
-        recipeInfo.readyInMinutes = verification.checkNumber(recipeInfo.readyInMinutes);
-        if(recipeInfo.sourceURL) recipeInfo.sourceURL = verification.checkURL(recipeInfo.sourceURL);
-        if(recipeInfo.certified !== true) recipeInfo.certified = false;
+      recipeInfo.title = verification.checkOnlyWordsString(recipeInfo.title);
+      recipeInfo.userId = verification.checkId(recipeInfo.userId);
+      recipeInfo.flavors = verification.checkOnlyWordsStringArray(
+        recipeInfo.flavors
+      );
+      recipeInfo.servings = verification.checkNumber(recipeInfo.servings);
 
-        const newRecipe = await recipesData.createRecipe(recipeInfo.userId, recipeInfo.title, recipeInfo.flavors, recipeInfo.imageURL, recipeInfo.ingredients, recipeInfo.instructions, recipeInfo.servings, recipeInfo.readyInMinutes, recipeInfo.sourceURL, recipeInfo.certified);
-        res.json(newRecipe);
+      if (recipeInfo.imageURL)
+        recipeInfo = verification.checkURL(recipeInfo.imageURL);
+      recipeInfo.ingredients = verification.checkStringArray(
+        recipeInfo.ingredients
+      );
+      recipeInfo.instructions = verification.checkOnlyWordsStringArray(
+        recipeInfo.instructions
+      );
+      recipeInfo.readyInMinutes = verification.checkNumber(
+        recipeInfo.readyInMinutes
+      );
+      if (recipeInfo.sourceURL)
+        recipeInfo.sourceURL = verification.checkURL(recipeInfo.sourceURL);
+      if (recipeInfo.certified !== true) recipeInfo.certified = false;
+
+      const newRecipe = await recipesData.createRecipe(
+        recipeInfo.userId,
+        recipeInfo.title,
+        recipeInfo.flavors,
+        recipeInfo.imageURL,
+        recipeInfo.ingredients,
+        recipeInfo.instructions,
+        recipeInfo.servings,
+        recipeInfo.readyInMinutes,
+        recipeInfo.sourceURL,
+        recipeInfo.certified
+      );
+      res.json(newRecipe);
     } catch (e) {
-        res.status(500).json({ error: e });
+      res.status(500).json({ error: e });
     }
-});
+  });
 
-router.get("/:id", async (req, res) => {
-    try{
-        const recipe = await recipesData.get(req.params.id);
-        res.json(recipe);
+router
+  .get("/:id", async (req, res) => {
+    try {
+      const recipe = await recipesData.get(req.params.id);
+      res.json(recipe);
+    } catch (e) {
+      res.status(404).json({ error: "Recipe not found" });
     }
-    catch(e){
-        res.status(404).json({ error: "Recipe not found" });
-    }
-}).post("/:id", async (req, res) => {
+  })
+  .post("/:id", async (req, res) => {
     const recipeInfo = req.body;
     if (!recipeInfo) {
-        res.status(400).json({ error: "You must provide data to create a recipe" });
-        return;
+      res
+        .status(400)
+        .json({ error: "You must provide data to create a recipe" });
+      return;
     }
     try {
-        recipeInfo.title = verification.checkOnlyWordsString(recipeInfo.title);
-        recipeInfo.userId = verification.checkId(recipeInfo.userId);
-        recipeInfo.flavors = verification.checkOnlyWordsStringArray(recipeInfo.flavors);
-        recipeInfo.servings = verification.checkNumber(recipeInfo.servings);
-        
-        if(recipeInfo.imageURL) recipeInfo = verification.checkURL(recipeInfo.imageURL);
-        recipeInfo.ingredients = verification.checkStringArray(recipeInfo.ingredients);
-        recipeInfo.instructions = verification.checkOnlyWordsStringArray(recipeInfo.instructions);
-        recipeInfo.readyInMinutes = verification.checkNumber(recipeInfo.readyInMinutes);
-        if(recipeInfo.sourceURL) recipeInfo.sourceURL = verification.checkURL(recipeInfo.sourceURL);
+      recipeInfo.title = verification.checkOnlyWordsString(recipeInfo.title);
+      recipeInfo.userId = verification.checkId(recipeInfo.userId);
+      recipeInfo.flavors = verification.checkOnlyWordsStringArray(
+        recipeInfo.flavors
+      );
+      recipeInfo.servings = verification.checkNumber(recipeInfo.servings);
 
-        const newRecipe = await recipesData.createRecipe(recipeInfo.userId, recipeInfo.title, recipeInfo.flavors, recipeInfo.imageURL, recipeInfo.ingredients, recipeInfo.instructions, recipeInfo.servings, recipeInfo.readyInMinutes, recipeInfo.sourceURL);
-        res.json(newRecipe);
+      if (recipeInfo.imageURL)
+        recipeInfo = verification.checkURL(recipeInfo.imageURL);
+      recipeInfo.ingredients = verification.checkStringArray(
+        recipeInfo.ingredients
+      );
+      recipeInfo.instructions = verification.checkOnlyWordsStringArray(
+        recipeInfo.instructions
+      );
+      recipeInfo.readyInMinutes = verification.checkNumber(
+        recipeInfo.readyInMinutes
+      );
+      if (recipeInfo.sourceURL)
+        recipeInfo.sourceURL = verification.checkURL(recipeInfo.sourceURL);
+
+      const newRecipe = await recipesData.createRecipe(
+        recipeInfo.userId,
+        recipeInfo.title,
+        recipeInfo.flavors,
+        recipeInfo.imageURL,
+        recipeInfo.ingredients,
+        recipeInfo.instructions,
+        recipeInfo.servings,
+        recipeInfo.readyInMinutes,
+        recipeInfo.sourceURL
+      );
+      res.json(newRecipe);
     } catch (e) {
-        res.status(500).json({ error: e });
+      res.status(500).json({ error: e });
     }
-});
+  });
 //TODO -- DELETE
 
-router.get("/filter" , async (req, res) => {
+router.get(
+  "/filter",
+  async (req, res) => {
     try {
-        const recipes = await recipesData.getRecipesByFilter(req.body.filter);
-        return res.json(recipes);
+      const recipes = await recipesData.getRecipesByFilter(req.body.filter);
+      return res.json(recipes);
     } catch (e) {
-        res.status(500).send();
+      res.status(500).send();
     }
-}
+  }
 
-//TODO : FILTER 
+  //TODO : FILTER
 );
 
 export default router;
